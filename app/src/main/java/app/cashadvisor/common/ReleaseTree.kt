84171c2c5c06243d84c7a5aa98dbb4abb183cfd9
Aleksandr Poliakov <master@artwist.ru>
2023-12-08 15:43:51 +0300

    added isLoggable + crashlytics.recordException

--- app/src/main/java/app/cashadvisor/common/ReleaseTree.kt
+++ app/src/main/java/app/cashadvisor/common/ReleaseTree.kt
@@ -8,9 +8,16 @@ import timber.log.Timber
 class ReleaseTree : Timber.Tree() {
     private val crashlytics = FirebaseCrashlytics.getInstance()
 
+    override fun isLoggable(tag: String?, priority: Int): Boolean {
+        return priority == WARN || priority == ERROR
+    }
+
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-        if (priority == WARN || priority == ERROR) {
-            crashlytics.log("\\$priority \\$tag \\$message")
+        when (priority) {
+            WARN -> {crashlytics.log("$priority $tag $message")
+                crashlytics.recordException(t ?: Exception(message))
+            }
+            ERROR -> crashlytics.recordException(t ?: Exception(message))
         }
     }
 }
