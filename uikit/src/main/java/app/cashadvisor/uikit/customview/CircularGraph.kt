bd225ff9cc12d480bdab9e1c314bde028b174f2b
vladalexeco <vladalexeco86@gmail.com>
2023-12-17 11:40:18 +0300

    feat: added new attributes such as fontFamily, headerTextColor, valueTextColor, currencySymbol

--- uikit/src/main/java/app/cashadvisor/uikit/customview/CircularGraph.kt
+++ uikit/src/main/java/app/cashadvisor/uikit/customview/CircularGraph.kt
@@ -4,34 +4,37 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.util.AttributeSet
-import android.view.View
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.res.ResourcesCompat
 import app.cashadvisor.uikit.R
 
 class CircularGraph(
     context: Context,
     attrs: AttributeSet? = null,
-) : View(context, attrs) {
+) : AppCompatTextView(context, attrs) {
 
     private val paint1 = Paint(Paint.ANTI_ALIAS_FLAG)
     private val paint2 = Paint(Paint.ANTI_ALIAS_FLAG)
     private val textPaint1 = Paint(Paint.ANTI_ALIAS_FLAG)
     private val textPaint2 = Paint(Paint.ANTI_ALIAS_FLAG)
 
+    private var fontResourceId = UNDEFINED_FONT
     private var totalValueText = ""
     private var headerText = ""
-    private val rubleSymbol = "\u20BD"
-    private var strokeWidth = 36f
-
+    private var currencySymbol = context.getString(R.string.circular_graph_unknown_text)
+    private var strokeWidth = DEFAULT_WIDTH
 
     private var color1 = context.getColor(R.color.subcolour1)
-    private var color2 = context.getColor(R.color.m5)
+    private var color2 = context.getColor(R.color.m6)
+    private var textColor1 = context.getColor(R.color.colour1)
+    private var textColor2 = textColor1
 
-    private var textSize = 40f
+    private var textSize = DEFAULT_TEXT_SIZE
 
-    private var biasAngle = 7f
+    private var biasAngle = DEFAULT_BIAS_ANGLE
 
     private var startAngle1 = 0f
-    private var sweepAngle1 = 360f
+    private var sweepAngle1 = TWO_PI_ANGLE
     private var startAngle2 = 0f
     private var sweepAngle2 = 0f
 
@@ -42,10 +45,7 @@ class CircularGraph(
         paint2.style = Paint.Style.STROKE
         paint2.strokeCap = Paint.Cap.ROUND
 
-        textPaint1.color = context.getColor(R.color.subcolour2)
         textPaint1.textAlign = Paint.Align.CENTER
-
-        textPaint2.color = context.getColor(R.color.colour1)
         textPaint2.textAlign = Paint.Align.CENTER
 
         if (attrs != null) {
@@ -54,9 +54,13 @@ class CircularGraph(
             color1 = typedArray.getColor(R.styleable.CircularGraph_cg_varSourceArcColor, color1)
             color2 = typedArray.getColor(R.styleable.CircularGraph_cg_constSourceArcColor, color2)
             strokeWidth = typedArray.getDimension(R.styleable.CircularGraph_cg_circleThickness, strokeWidth)
-            biasAngle = typedArray.getDimension(R.styleable.CircularGraph_cg_arcSpacing, biasAngle)
+            biasAngle = typedArray.getFloat(R.styleable.CircularGraph_cg_arcSpacing, biasAngle)
             headerText = typedArray.getString(R.styleable.CircularGraph_cg_headerText) ?: headerText
-            textSize = typedArray.getDimension(R.styleable.CircularGraph_cg_headerTextSize, textSize)
+            textSize = typedArray.getDimension(R.styleable.CircularGraph_cg_captionTextSize, textSize)
+            currencySymbol = typedArray.getString(R.styleable.CircularGraph_cg_currencySymbol) ?: currencySymbol
+            fontResourceId = typedArray.getResourceId(R.styleable.CircularGraph_android_fontFamily, fontResourceId)
+            textColor1 = typedArray.getColor(R.styleable.CircularGraph_cg_headerTextColor, textColor1)
+            textColor2 = typedArray.getColor(R.styleable.CircularGraph_cg_valueTextColor, textColor2)
 
             typedArray.recycle()
         }
@@ -73,6 +77,16 @@ class CircularGraph(
 
         textPaint1.textSize = textSize
         textPaint2.textSize = textSize
+
+        if (fontResourceId != UNDEFINED_FONT) {
+            val customFont = ResourcesCompat.getFont(context, fontResourceId)
+            textPaint1.typeface = customFont
+            textPaint2.typeface = customFont
+        }
+
+        textPaint1.color = textColor1
+        textPaint2.color = textColor2
+
 
         val centerX = width / 2f
         val centerY = height / 2f
@@ -119,16 +133,16 @@ class CircularGraph(
         val varValueRatio = varValue / sum
         val constValueRatio = constValue / sum
 
-        totalValueText = "$rubleSymbol $sumFormat"
+        totalValueText = "$currencySymbol $sumFormat"
 
         val varValueAngle = varValueRatio * (360 - 2 * biasAngle)
         val constValueAngle = constValueRatio * (360 - 2 * biasAngle)
 
-        if (constValueRatio <= 0.01) {
+        if (constValueAngle <= biasAngle) {
             startAngle1 = 0f + biasAngle
             sweepAngle1 = varValueAngle
             sweepAngle2 = 0f
-        } else if (varValueRatio <= 0.01) {
+        } else if (varValueAngle <= biasAngle) {
             startAngle1 = 0f
             sweepAngle1 = 0f
             startAngle2 = 0f + biasAngle
@@ -142,6 +156,12 @@ class CircularGraph(
 
         invalidate()
     }
+
+    companion object {
+        private const val UNDEFINED_FONT = -1
+        private const val DEFAULT_WIDTH = 36f
+        private const val DEFAULT_TEXT_SIZE = 40f
+        private const val DEFAULT_BIAS_ANGLE = 7f
+        private const val TWO_PI_ANGLE = 360f
+    }
 }
-
-
