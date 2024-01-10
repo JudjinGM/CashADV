2e97525444a7ff5cdd0074aea7aac058d7164cc6
Aleksandr Poliakov <master@artwist.ru>
2024-01-10 20:26:34 +0300

    optimize imports

--- app/src/main/java/app/cashadvisor/authorization/presentation/ui/diagrams/UikitSampleFragment.kt
+++ app/src/main/java/app/cashadvisor/authorization/presentation/ui/diagrams/UikitSampleFragment.kt
@@ -1,17 +1,10 @@
 package app.cashadvisor.authorization.presentation.ui.diagrams
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import app.cashadvisor.BuildConfig.BUILD_TYPE
 import app.cashadvisor.R
 import app.cashadvisor.common.ui.BaseFragment
 import app.cashadvisor.common.ui.BaseViewModel
 import app.cashadvisor.databinding.FragmentUikitSampleBinding
-import com.google.firebase.BuildConfig
 
 class UikitSampleViewModel : BaseViewModel() {
     // example viewModel
