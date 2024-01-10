4cdfc5bf4d1c498c78c2ef60cd82da12bf794f39
Ilnur Valiev <102486283+JudjinGM@users.noreply.github.com>
2024-01-11 00:23:00 +0400

    Optimize imports

--- app/src/main/java/app/cashadvisor/authorization/presentation/ui/PinCodeFragment.kt
+++ app/src/main/java/app/cashadvisor/authorization/presentation/ui/PinCodeFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import app.cashadvisor.R
-import app.cashadvisor.databinding.FragmentLoginBinding
 import app.cashadvisor.databinding.FragmentPinCodeBinding
 
 class PinCodeFragment : Fragment() {
