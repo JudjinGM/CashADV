144aa3e020f3b896373df47cf1eb3feec4205a39
Aleksandr Poliakov <master@artwist.ru>
2024-01-11 21:10:11 +0300

    some code fixes

--- app/src/main/java/app/cashadvisor/authorization/presentation/ui/domain/models/PasswordInputData.kt
+++ app/src/main/java/app/cashadvisor/authorization/presentation/ui/domain/models/PasswordInputData.kt
@@ -3,12 +3,13 @@ package app.cashadvisor.authorization.presentation.ui.domain.models
 data class PasswordInputData(
     val password: String
 ) : BaseInputData() {
-    override fun isValid(): Boolean {
-        return password.length >= MIN_LENGTH
-    }
+    override fun isValid(): Boolean = password.length >= MIN_LENGTH && isCorrectSymbols()
+
+    private fun isCorrectSymbols(): Boolean = password.matches(Regex(REGEX_PATTERN))
 
     companion object {
         const val MIN_LENGTH = 8
+        const val REGEX_PATTERN = "[a-zA-Z0-9_]+"
     }
 }
 
--- app/src/main/java/app/cashadvisor/authorization/presentation/ui/domain/models/VerifycationCodeInputData.kt
+++ app/src/main/java/app/cashadvisor/authorization/presentation/ui/domain/models/VerifycationCodeInputData.kt
@@ -3,12 +3,9 @@ package app.cashadvisor.authorization.presentation.ui.domain.models
 data class VerifycationCodeInputData(
     val code: String
 ) : BaseInputData() {
-    override fun isValid(): Boolean {
-        return (code.length == CODE_LENGTH) && isNumbersOnly()
-    }
-    private fun isNumbersOnly(): Boolean {
-        return code.matches(Regex(REGEX_PATTERN))
-    }
+    override fun isValid(): Boolean = (code.length == CODE_LENGTH) && isNumbersOnly()
+
+    private fun isNumbersOnly(): Boolean = code.matches(Regex(REGEX_PATTERN))
 
     companion object {
         const val CODE_LENGTH = 4
