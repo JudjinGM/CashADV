9e7d209c79de010641501b103f392d573c82a26a
Aleksandr Poliakov <master@artwist.ru>
2024-01-14 19:01:23 +0300

    clearify comment for BaseInputData

--- app/src/main/java/app/cashadvisor/authorization/presentation/ui/domain/models/BaseInputData.kt
+++ app/src/main/java/app/cashadvisor/authorization/presentation/ui/domain/models/BaseInputData.kt
@@ -1,7 +1,7 @@
 package app.cashadvisor.authorization.presentation.ui.domain.models
 
 abstract class BaseInputData {
-    // todo возможно стоит возвращать результат валидации вместо булевого значения с указанием на ошибку
 
+    // todo возможно стоит вместо Boolean возвращать результат валидации с указанием на ошибку
     abstract fun isValid(): Boolean
 }
