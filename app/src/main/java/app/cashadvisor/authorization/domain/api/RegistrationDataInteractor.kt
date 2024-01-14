b393b564f9fb28f5649a36dc01c24a3c6ea824b5
Aleksandr Poliakov <master@artwist.ru>
2024-01-14 19:20:23 +0300

    creates methods to password reset

--- app/src/main/java/app/cashadvisor/authorization/domain/api/RegistrationDataInteractor.kt
+++ app/src/main/java/app/cashadvisor/authorization/domain/api/RegistrationDataInteractor.kt
@@ -1,10 +1,13 @@
 package app.cashadvisor.authorization.domain.api
 
 import app.cashadvisor.authorization.domain.models.LoginData
+import app.cashadvisor.authorization.domain.models.PasswordInputData
 import app.cashadvisor.common.models.NetworkResult
 import kotlinx.coroutines.flow.Flow
 
 interface RegistrationDataInteractor {
     suspend fun submitLoginData(data: LoginData): Flow<NetworkResult<Boolean>>
     fun isLoginDataValid(data: LoginData): Boolean
+    suspend fun requestPasswordReset(): Flow<NetworkResult<Boolean>>
+    suspend fun setNewPassword(data: PasswordInputData): Flow<NetworkResult<Boolean>>
 }
