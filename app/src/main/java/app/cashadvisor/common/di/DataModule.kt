9f135bc4c2630554f44ef081821a9418f7ae2773
Aleksandr Poliakov <master@artwist.ru>
2024-02-01 20:43:36 +0300

    configure data module di

---
+++ app/src/main/java/app/cashadvisor/common/di/DataModule.kt
@@ -0,0 +1,57 @@
+package app.cashadvisor.common.di
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
+import app.cashadvisor.authorization.data.impl.CredentialsStorageImpl
+import app.cashadvisor.authorization.domain.api.CredentialsStorage
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DataModule {
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson = Gson()
+
+    @Provides
+    @Singleton
+    fun providesMasterKey(): String =
+        MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+
+    @Provides
+    @Singleton
+    fun providesEncriptedSharedPreferences(@ApplicationContext context: Context): SharedPreferences {
+        val masterKey = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+        return EncryptedSharedPreferences.create(
+            SECRET_SETTINGS,
+            masterKey,
+            context,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun providesCredintialsStorage(
+        @ApplicationContext context: Context,
+        storage: SharedPreferences = providesEncriptedSharedPreferences(context),
+        key: String = CREDENTIALS_KEY,
+        gson: Gson = provideGson()
+    ): CredentialsStorage = CredentialsStorageImpl(storage, key, gson)
+
+    companion object {
+        private const val SECRET_SETTINGS = "secret_shared_prefs"
+        private const val CREDENTIALS_KEY = "secret_credentials"
+    }
+}
