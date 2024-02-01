243b781dc132a802a1d7bd78ef9027cfa761c1f2
Aleksandr Poliakov <master@artwist.ru>
2024-02-01 21:22:21 +0300

    deletes unusual comment

--- app/src/main/java/app/cashadvisor/authorization/domain/models/AccountInformation.kt
+++ app/src/main/java/app/cashadvisor/authorization/domain/models/AccountInformation.kt
@@ -3,7 +3,6 @@ package app.cashadvisor.authorization.domain.models
 sealed class AccountInformation {
     object NotAuthorized : AccountInformation()
     data class Authorized(
-        // todo пока модель черновая, перепроверить что тут должно быть
         val accessToken: String,
         val refreshToken: String
     ) : AccountInformation()
