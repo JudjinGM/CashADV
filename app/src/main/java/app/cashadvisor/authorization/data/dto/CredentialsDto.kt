092285578ad4278763f425838b91462ecc87a543
Aleksandr Poliakov <master@artwist.ru>
2024-02-08 21:42:04 +0300

    adds Serializable annotation to CredentialsDto

--- app/src/main/java/app/cashadvisor/authorization/data/dto/CredentialsDto.kt
+++ app/src/main/java/app/cashadvisor/authorization/data/dto/CredentialsDto.kt
@@ -2,7 +2,9 @@ package app.cashadvisor.authorization.data.dto
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Serializable
 
+@Serializable
 @Parcelize
 data class CredentialsDto(
     val accessToken: String,
