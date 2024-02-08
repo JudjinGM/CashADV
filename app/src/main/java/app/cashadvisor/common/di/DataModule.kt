7048245e35ed6e186defb45bf1937e001a3e12b6
Aleksandr Poliakov <master@artwist.ru>
2024-02-08 22:06:30 +0300

    renames auth data module

--- app/src/main/java/app/cashadvisor/common/di/DataModule.kt
+++ app/src/main/java/app/cashadvisor/common/di/DataModule.kt
@@ -1,15 +1,8 @@
 package app.cashadvisor.common.di
 
-import android.content.Context
-import android.content.SharedPreferences
-import androidx.security.crypto.EncryptedSharedPreferences
-import androidx.security.crypto.MasterKeys
-import app.cashadvisor.authorization.data.impl.CredentialsStorageImpl
-import app.cashadvisor.authorization.domain.api.CredentialsStorage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.json.Json
 import javax.inject.Singleton
