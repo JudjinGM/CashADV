bfffda862bf15aff0ceafc36c06dc439911f2964
Aleksandr Poliakov <master@artwist.ru>
2024-02-08 22:04:16 +0300

    splits DataModule at common and feature parts

--- app/src/main/java/app/cashadvisor/common/di/DataModule.kt
+++ app/src/main/java/app/cashadvisor/common/di/DataModule.kt
@@ -18,40 +18,8 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 class DataModule {
-
     @Provides
     @Singleton
     fun provideJson(): Json = Json { ignoreUnknownKeys = true }
 
-    @Provides
-    @Singleton
-    fun providesMasterKey(): String =
-        MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
-
-    @Provides
-    @Singleton
-    fun providesEncriptedSharedPreferences(@ApplicationContext context: Context): SharedPreferences {
-        val masterKey = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
-        return EncryptedSharedPreferences.create(
-            SECRET_SETTINGS,
-            masterKey,
-            context,
-            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-        )
-    }
-
-    @Provides
-    @Singleton
-    fun providesCredintialsStorage(
-        @ApplicationContext context: Context,
-        storage: SharedPreferences = providesEncriptedSharedPreferences(context),
-        key: String = CREDENTIALS_KEY,
-        gson: Json = provideJson()
-    ): CredentialsStorage = CredentialsStorageImpl(storage, key, gson)
-
-    companion object {
-        private const val SECRET_SETTINGS = "secret_shared_prefs"
-        private const val CREDENTIALS_KEY = "secret_credentials"
-    }
 }
