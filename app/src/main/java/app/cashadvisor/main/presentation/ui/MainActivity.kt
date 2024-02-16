8b3970f465f039fdb26afcc853e7c75904d04793
Aleksandr Poliakov <master@artwist.ru>
2024-02-16 14:00:54 +0300

    adds status bar tint

--- app/src/main/java/app/cashadvisor/main/presentation/ui/MainActivity.kt
+++ app/src/main/java/app/cashadvisor/main/presentation/ui/MainActivity.kt
@@ -1,11 +1,16 @@
 package app.cashadvisor.main.presentation.ui
 
+import android.content.res.Configuration
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.WindowInsetsController
+import android.view.WindowManager
 import android.widget.Button
 import androidx.activity.viewModels
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -38,7 +43,7 @@ class MainActivity : AppCompatActivity() {
         Timber.tag("MainActivity").e("Error log")
 
         binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
-
+        setStatusBarColor()
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
@@ -137,5 +142,31 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    /**
+     * Метод определяет теккущую тему устройства (тёмная/светлая) и относительно этого устанавливает цвета в статус баре
+     */
+    private fun setStatusBarColor() {
+        val uiMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+
+        if (uiMode == Configuration.UI_MODE_NIGHT_NO) {
+            window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                window.insetsController?.setSystemBarsAppearance(
+                    WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                    WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+                )
+            } else {
+                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            }
+        } else {
+            window.statusBarColor = ContextCompat.getColor(this, R.color.black)
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+                window.decorView.systemUiVisibility = 0
+            }
+        }
     }
 }
