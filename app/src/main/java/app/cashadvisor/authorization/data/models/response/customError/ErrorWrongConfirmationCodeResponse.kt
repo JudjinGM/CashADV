10ecb0f6d4e1753f29e3a0ab4112311b2c4a310b
Ilnur Valiev <102486283+JudjinGM@users.noreply.github.com>
2024-03-14 20:11:32 +0400

    fixed backend changes

--- app/src/main/java/app/cashadvisor/authorization/data/models/response/customError/ErrorWrongConfirmationCodeResponse.kt
+++ app/src/main/java/app/cashadvisor/authorization/data/models/response/customError/ErrorWrongConfirmationCodeResponse.kt
@@ -1,10 +1,12 @@
 package app.cashadvisor.authorization.data.models.response.customError
 
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class ErrorWrongConfirmationCodeResponse(
-    @SerialName("remaining_attempts") val remainingAttempts: Int,
-    @SerialName("lock_duration") val lockDuration: Int,
+    @SerialName("remaining_attempts") val remainingAttempts: Long,
+    @SerialName("lock_duration") val lockDuration: Long,
     val error: String,
-    @SerialName("status_code") val statusCode: Int
+    @SerialName("status_code") val statusCode: Long
 )
